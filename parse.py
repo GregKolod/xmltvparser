87ff868b7178978d3802ddb08f8a13c512dc7296
Stephen McCullough <stephen@skillfulgorilla.com>
2015-03-16 12:48:35 +0000

    Created parse_broadcasts. Renamed channels to parse_channels.

--- parse.py
+++ parse.py
@@ -11,7 +11,14 @@ Channel = namedtuple('Channel', [
     'id', 'name', 'icon'
 ])
 
-def channels():
+def parse_args():
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+        help="location of xml", metavar="FILE")
+
+    return parser.parse_args()
+
+def parse_channels():
     channels = {}
     for key in xmltv.read_channels(open(filename, 'r')):
         name = map(itemgetter(0), key['display-name'])
@@ -28,24 +35,21 @@ def channels():
 def retrieve_channel(id):
     return CHANNELS[id]
 
+def parse_broadcasts():
+    for key in xmltv.read_programmes(open(filename, 'r')):
+        channel = retrieve_channel(key['channel'])
+        titles = map(itemgetter(0), key['title'])
+
+        print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop']))
+
 def format_time(timestamp):
     return datetime.datetime.strptime(timestamp[:12], "%Y%m%d%H%M%S")
-
-def parse_args():
-    parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename",
-        help="location of xml", metavar="FILE")
-
-    return parser.parse_args()
 
 if __name__ == "__main__":
     (options, args) = parse_args()
     if os.path.exists(options.filename):
         filename = options.filename
 
-    CHANNELS = channels()
-    for key in xmltv.read_programmes(open(filename, 'r')):
-        channel = retrieve_channel(key['channel'])
-        titles = map(itemgetter(0), key['title'])
+    CHANNELS = parse_channels()
+    parse_broadcasts()
 
-        print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop']))
