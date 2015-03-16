3f2ef672dceb79cfcbf24f29ae5e01aac9798e4f
Stephen McCullough <stephen@skillfulgorilla.com>
2015-03-16 13:37:09 +0000

    Added parse_broadcasts and retrieve_broadcast

--- parse.py
+++ parse.py
@@ -36,11 +36,25 @@ def retrieve_channel(id):
     return CHANNELS[id]
 
 def parse_broadcasts():
-    for key in xmltv.read_programmes(open(filename, 'r')):
-        channel = retrieve_channel(key['channel'])
-        titles = map(itemgetter(0), key['title'])
+    broadcasts = {}
+    ctr = 0
+    for element in xmltv.read_programmes(open(filename, 'r')):
+        ctr += 1
+        broadcasts[ctr] = retrieve_broadcast(element)
 
-        print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop']))
+    return broadcasts
+
+def retrieve_broadcast(element):
+        #titles = map(itemgetter(0), element['title'])
+
+        return {
+            'channel': retrieve_channel(element['channel']),
+            #'blurb': _retrieve_blurb(element),
+            #'title': _retrieve_title(element),
+            'start_time': format_time(element['start']),
+            'end_time': format_time(element['stop']),
+        }
+        #print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop']))
 
 def format_time(timestamp):
     return datetime.datetime.strptime(timestamp[:12], "%Y%m%d%H%M%S")
@@ -51,5 +65,4 @@ if __name__ == "__main__":
         filename = options.filename
 
     CHANNELS = parse_channels()
-    parse_broadcasts()
-
+    BROADCASTS = parse_broadcasts()
