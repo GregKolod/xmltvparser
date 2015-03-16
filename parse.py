5783c490f3bf3f6111c96d08e15b2d2680374577
Stephen McCullough <stephen@skillfulgorilla.com>
2015-03-16 11:49:07 +0000

    Amended indenting and trailing spaces

--- parse.py
+++ parse.py
@@ -15,16 +15,16 @@ Channel = namedtuple('Channel', [
 def channels():
     channels = {}
     for key in xmltv.read_channels(open(filename, 'r')):
-          name = map(itemgetter(0), key['display-name'])
-          id   = key['id']
-          src  = key['icon'][0]['src']
-          name = name[0]
+        name = map(itemgetter(0), key['display-name'])
+        id   = key['id']
+        src  = key['icon'][0]['src']
+        name = name[0]
 
-          rec = dict(zip(Channel._fields, [id, name, src]))
-          channel = Channel(**rec)
-          channels[channel.id] = channel
+        rec = dict(zip(Channel._fields, [id, name, src]))
+        channel = Channel(**rec)
+        channels[channel.id] = channel
 
-    return channels 
+    return channels
 
 CHANNELS = channels()
 
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     for key in xmltv.read_programmes(open(filename, 'r')):
         channel = create_channel(key['channel'])
         titles = map(itemgetter(0), key['title'])
-        
-        print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop'])) 
+
+        print "%s - %s - %s - %s" % (titles[0], channel.name, format_time(key['start']), format_time(key['stop']))
